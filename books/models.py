2f28ee8c301ca2802132a66c9020d357a5d428c7
WanjaMIKE <38399942+WanjaMIKE@users.noreply.github.com>
2023-05-14 09:30:07 +0300

    Django API

--- books/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    name = models.CharField(max_length=200)
-    description= models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.name + ' ' + self.description
--- books/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-from .models import Book
-from .serializers import BookSerializer
-
-def book_list(request):
-    books = Book.objects.all()
-    bookserializer = BookSerializer(books, many=True)
-    return JsonResponse({'drinks':bookserializer.data})
